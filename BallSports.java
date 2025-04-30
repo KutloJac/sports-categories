b2d6f5c5fe4ed45ece3e5f63fb8683630ba5ef2c
waronamodisaotsile <waronamodisaotsile16@gmail.com>
2025-04-30 21:28:06 +0200

    Update BallSports.java

--- BallSports.java
+++ BallSports.java
@@ -1,19 +1,38 @@
-public class BallsSports extends Sportss{
-        private String ballType;
-        private boolean isTeamSport;
+public class BallSports extends Sports {
+    private String ballType;
+    private boolean isTeamSport;
+
+    public BallSports(String name, int playerCount, boolean isPopularInBotswana, String ballType, boolean isTeamSport) throws InvalidPlayerCountException {
+        super(name, playerCount, isPopularInBotswana);
+        this.ballType = ballType;
+        this.isTeamSport = isTeamSport;
+    }
     
-        public BallsSports(String name, int playerCount, boolean isPopularInBotswana, String ballType, boolean isTeamSport) throws InvalidPlayerCountException{
-            super(name, playerCount, isPopularInBotswana);
-            this.ballType = ballType;
-            this.isTeamSport = isTeamSport;
-            
-        }
-        
-        // Polymorphism: Override rules
-        @Override
-        public void displayRules() {
-            System.out.println("Rule: " + ballType + "ball. Team sport? " + isTeamSport);
-        }
+    public String getBallType() {
+        return ballType;
+    }
     
+    public void setBallType(String ballType) {
+        this.ballType = ballType;
+    }
     
+    public boolean isTeamSport() {
+        return isTeamSport;
+    }
+    
+    public void setTeamSport(boolean isTeamSport) {
+        this.isTeamSport = isTeamSport;
+    }
+    
+    // Polymorphism: Override rules
+    @Override
+    public void displayRules() {
+        System.out.println("Rule: " + ballType + " ball. Team sport? " + (isTeamSport ? "Yes" : "No"));
+    }
+    
+    // Implementation of interface method
+    @Override
+    public String getCategory() {
+        return "Ball Sport";
+    }
 }
